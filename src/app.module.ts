a74e0461a94018f3266971e45b98b4387d3a7f68
otabek0311 <abdullayevotabek414@gmail.com>
2025-12-11 11:17:36 +0500

    122

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true}),    
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string), StudentModule,
+  ],
   controllers: [],
   providers: [],
 })
